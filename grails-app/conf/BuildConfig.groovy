5ce3736af184a051d60946454126069f327c9d41
Owen Rubel <orubel@gmail.com>
2014-07-29 14:07:55 -0700

    Update BuildConfig.groovy
    fixing config to make sure springsecurity is added

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -56,6 +56,8 @@ grails.project.dependency.resolution = {
         runtime ":database-migration:1.3.8"
         runtime ":jquery:1.10.2.2"
         //runtime ":resources:1.2.1"
+        
+        compile ':spring-security-core:2.0-RC3'
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0.1"
